0beb04fe59093a5d1b5d0fdf5e2a7007b0278685
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-01-20 11:26:08 +0100

    Auction and CreateAuction improvements made into class components map panning/positioning drawing active place polygon and various little things

--- src/components/Auction.tsx
+++ src/components/Auction.tsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
-import { MapContainer, ImageOverlay } from 'react-leaflet'
+import { MapContainer, ImageOverlay, Circle, Polygon } from 'react-leaflet'
 import L from 'leaflet';
 import './Auction.css'
 import 'leaflet/dist/leaflet.css';
-import { useEffect, useState } from 'react';
 import { mutezToTez } from '../tz/Utils';
 import Contracts from '../tz/Contracts';
+import { MapSetCenter } from '../forms/CreateAuction';
+import React from 'react';
+import Metadata from '../world/Metadata';
 
 type AuctionProps = {
     auctionId: number;
@@ -14,81 +16,135 @@ type AuctionProps = {
     startTime: number; // in mutez
     endTime: number;
     owner: string;
-    tokenId: string;
+    tokenId: number;
     reloadAuctions(): void;
     // using `interface` is also ok
     //message: string;
-  };
+};
 
-export default function Auction(props: AuctionProps) {
-    const [updateCount, setUpdateCount] = useState(0);
+type AuctionState = {
+    updateCount: number,
+    mapLocation: [number, number],
+    placePoly: [number, number][]
+}
 
-    const duration = props.endTime - props.startTime;
-    const current_time = Math.floor(Date.now() / 1000);
-    const started = current_time >= props.startTime;
-    const since_start = Math.min(current_time, props.endTime) - props.startTime;
-    const progress = 100 - Math.floor(since_start / duration * 100);
+export default class Auction extends React.Component<AuctionProps, AuctionState> {
+    constructor(props: AuctionProps) {
+        super(props);
+        this.state = {
+            updateCount: 0,
+            mapLocation: [500, 500],
+            placePoly: []
+        };
+        this.updateTimeVars();
+    }
+
+    private duration: number = this.props.endTime - this.props.startTime;
+    private current_time: number = 0;
+    private started: boolean = false;
+    private since_start: number = 0;
+    private progress: number = 0;
+
+    private refreshInterval: NodeJS.Timeout | null = null;
+    private reloadInterval: NodeJS.Timeout | null = null;
+
+    private updateTimeVars() {
+        this.current_time = Math.floor(Date.now() / 1000);
+        this.started = this.current_time >= this.props.startTime;
+        this.since_start = Math.min(this.current_time, this.props.endTime) - this.props.startTime;
+        this.progress = Math.min(100 - this.since_start / this.duration * 100, 100);
+
+        console.log(this.progress);
+    }
 
     // returns current price in mutez
-    const calculateCurrentPrice = (): number => {
-        if(current_time >= props.endTime) return props.endPrice;
+    private calculateCurrentPrice(): number {
+        if(this.current_time >= this.props.endTime) return this.props.endPrice;
 
         const granularity = 60; // seconds
         // From the auction contract code.
         // Always to simulate integer division.
-        const duration_g = Math.floor(duration / granularity);
-        const time_since_start_g = Math.floor(since_start / granularity);
-        const mutez_per_interval = Math.floor((props.startPrice - props.endPrice) / duration_g);
+        const duration_g = Math.floor(this.duration / granularity);
+        const time_since_start_g = Math.floor(this.since_start / granularity);
+        const mutez_per_interval = Math.floor((this.props.startPrice - this.props.endPrice) / duration_g);
         const time_deduction = mutez_per_interval * time_since_start_g;
 
-        const current_price = props.startPrice - time_deduction;
+        const current_price = this.props.startPrice - time_deduction;
         return current_price;
     }
 
-    const bidOnAuction = async () => {
-        await Contracts.bidOnAuction(props.auctionId, calculateCurrentPrice());
+    private async bidOnAuction() {
+        await Contracts.bidOnAuction(this.props.auctionId, this.calculateCurrentPrice());
         
         // Wait a little for the indexer to catch up.
-        // TODO: figure out if what happens when you navigate away from
-        // the page before timeout triggers.
-        setTimeout(() => {
-            props.reloadAuctions();
+        this.reloadInterval = setTimeout(() => {
+            this.props.reloadAuctions();
         }, 5000);
     }
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-          setUpdateCount(updateCount + 1);
+    private panMapToPlace(place_id: number) {
+        Metadata.getPlaceMetadata(place_id).then((res) => {
+            const coords = res.token_info.center_coordinates;
+            const center_pos: [number, number] = [500 + -coords[2], 500 + coords[0]];
+
+            const polygon = res.token_info.border_coordinates;
+            const placePoly: [number, number][] = []
+            for(const pos of polygon)
+            {
+                placePoly.push([center_pos[0] + -pos[2], center_pos[1] + pos[0]]);
+            }
+
+            this.setState({ mapLocation: center_pos, placePoly: placePoly });
+        })
+    }
+
+    componentDidMount() {
+        // set Interval
+        this.refreshInterval = setInterval(() => {
+            this.updateTimeVars();
+            this.setState({ updateCount: this.state.updateCount + 1 });
         }, 10000);
-        return () => clearInterval(interval);
-    });
 
-    return (
-        <div className="m-3 Auction">
-            <div className='p-3'>
-                <img className="mx-auto mb-1 d-block" src="/logo192.png" alt="" width="48" height="48" />
-                <h4 className="text-center mb-0">Place #{props.tokenId}</h4>
-                <small className='text-center d-block mb-0'>Auction #{props.auctionId}</small>
-            </div>
-            <MapContainer className="auction-img" center={[500, 500]} zoom={1} attributionControl={false} dragging={false} zoomControl={false} scrollWheelZoom={false} crs={L.CRS.Simple} alt="A preview map of the land">
-                <ImageOverlay bounds={[[0,0], [1000, 1000]]} url="/img/map.svg" />
-            </MapContainer>
-            <div className='p-3'>
-                <div className="progress mb-3">
-                    <div id="auctionProgress" className="progress-bar bg-primary" role="progressbar" style={{ width: `${progress}%` }} aria-valuemin={0} aria-valuemax={100} aria-valuenow={progress}></div>
+        this.panMapToPlace(this.props.tokenId);
+    }
+    
+    componentWillUnmount() {
+        // Clear the interval right before component unmount
+        if(this.refreshInterval) clearInterval(this.refreshInterval);
+        if(this.reloadInterval) clearInterval(this.reloadInterval);
+    }
+
+    render() {
+        return (
+            <div className="m-3 Auction">
+                <div className='p-3'>
+                    <img className="mx-auto mb-1 d-block" src="/logo192.png" alt="" width="48" height="48" />
+                    <h4 className="text-center mb-0">Place #{this.props.tokenId}</h4>
+                    <small className='text-center d-block mb-0'>Auction #{this.props.auctionId}</small>
                 </div>
+                <MapContainer className="auction-img" center={[500, 500]} zoom={1} attributionControl={false} dragging={false} zoomControl={false} scrollWheelZoom={false} crs={L.CRS.Simple} alt="A preview map of the land">
+                    <ImageOverlay bounds={[[0, 0], [1000, 1000]]} url="/img/map.svg" />
+                    <MapSetCenter center={this.state.mapLocation} animate={false} />
+                    <Circle center={this.state.mapLocation} radius={1.5} color='#d58195' fillColor='#d58195' fill={true} fillOpacity={1} />
+                    <Polygon positions={this.state.placePoly} color='#d58195' weight={10} lineCap='square'/>
+                </MapContainer>
+                <div className='p-3'>
+                    <div className="progress mb-3">
+                        <div id="auctionProgress" className="progress-bar bg-primary" role="progressbar" style={{ width: `${this.progress}%` }} aria-valuemin={0} aria-valuemax={100} aria-valuenow={this.progress}></div>
+                    </div>
 
-                <p className='small'>
-                    Land area: 100 m<sup>2</sup><br/>
-                    Current owner: <a href={`https://tzkt.io/${props.owner}`} target='_blank' rel='noreferrer'>{props.owner.substring(0,12)}...</a><br/>
-                    Start price: {mutezToTez(props.startPrice).toNumber()} &#42793;<br/>
-                    End price: {mutezToTez(props.endPrice).toNumber()} &#42793;<br/>
-                    Duration: {duration / 3600}h
-                </p>
+                    <p className='small'>
+                        Land area: 100 m<sup>2</sup><br/>
+                        Current owner: <a href={`https://tzkt.io/${this.props.owner}`} target='_blank' rel='noreferrer'>{this.props.owner.substring(0,12)}...</a><br/>
+                        Start price: {mutezToTez(this.props.startPrice).toNumber()} &#42793;<br/>
+                        End price: {mutezToTez(this.props.endPrice).toNumber()} &#42793;<br/>
+                        Duration: {this.duration / 3600}h
+                    </p>
 
-                <Link to='/explore?coordx=10&coordz=10' className="btn btn-outline-secondary btn-sm w-100 mb-1">Visit place</Link>
-                <button onClick={bidOnAuction} className="btn btn-primary btn-md w-100" disabled={!started}>{!started ? "Not started" : "Get for ~" + mutezToTez(calculateCurrentPrice()).toNumber().toFixed(2) + " \uA729"}</button>
+                    <Link to='/explore?coordx=10&coordz=10' className="btn btn-outline-secondary btn-sm w-100 mb-1">Visit place</Link>
+                    <button onClick={this.bidOnAuction.bind(this)} className="btn btn-primary btn-md w-100" disabled={!this.started}>{!this.started ? "Not started" : "Get for ~" + mutezToTez(this.calculateCurrentPrice()).toNumber().toFixed(2) + " \uA729"}</button>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
--- src/forms/CreateAuction.tsx
+++ src/forms/CreateAuction.tsx
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     Formik,
     Form,
     Field
 } from 'formik';
-import { MapContainer, ImageOverlay } from 'react-leaflet'
-import L from 'leaflet';
+import { MapContainer, ImageOverlay, useMap, Circle, Polygon } from 'react-leaflet'
+import L, { Map } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import BigNumber from 'bignumber.js';
 import DutchAuction from '../tz/DutchAuction';
 import { useNavigate } from 'react-router-dom';
+import Metadata from '../world/Metadata';
+
+type MapSetCenterProps = {
+    center: [number, number],
+    animate?: boolean
+}
+
+// Because react-leaflet is the height of retardation,
+// we make a function component that can pan the map.
+export const MapSetCenter: React.FC<MapSetCenterProps> = (props) => {
+    const parentMap = useMap();
+
+    useEffect(() => {
+        parentMap.panTo(props.center, { animate: props.animate });
+    });
+
+    return (
+        <div></div>
+    )
+}
 
 interface CreateAuctionFormValues {
     /*itemTitle: string;
@@ -25,91 +45,134 @@ interface CreateAuctionFormValues {
 type CreateAuctionFormProps = {}
 
 type CreateAuctionFormState = {
-    error: string
+    error: string,
+    mapLocation: [number, number],
+    placePoly: [number, number][],
 }
 
-export const CreateAuctionForm: React.FC<CreateAuctionFormProps> = (props) => {
-    const initialValues: CreateAuctionFormValues = { placeId: 0, duration: 48, startPrice: 0, endPrice: 0 };
-    const state: CreateAuctionFormState = { error: "" }
-    const navigate = useNavigate();
+export class CreateAuctionForm extends React.Component<CreateAuctionFormProps, CreateAuctionFormState> {
+    private initialValues: CreateAuctionFormValues = { placeId: 0, duration: 48, startPrice: 0, endPrice: 0 };
 
-    return (
-        <div className="container text-start pt-4">
-            <div className='row'>
-                <div className='col-lg-2 col-md-0'></div>
-                <div className='col-lg-4 col-md-6'>
-                    <h2>Create Auction</h2>
-                    <Formik
-                        initialValues={initialValues}
-                        onSubmit={async (values, actions) => {
-                            // clear error state
-                            state.error = '';
-                            //console.log({ values, actions });
-                            //alert(JSON.stringify(values, null, 2));
+    constructor(props: CreateAuctionFormProps) {
+        super(props);
+        this.state = {
+            error: '',
+            mapLocation: [500, 500],
+            placePoly: []
+        };
+    }
 
-                            // TODO: validation
+    private panMapToPlace(place_id: number) {
+        Metadata.getPlaceMetadata(place_id).then((res) => {
+            const coords = res.token_info.center_coordinates;
+            const center_pos: [number, number] = [500 + -coords[2], 500 + coords[0]];
 
-                            try {
-                                await DutchAuction.createAuction(new BigNumber(values.placeId), values.startPrice, values.endPrice, values.duration);
+            const polygon = res.token_info.border_coordinates;
+            const placePoly: [number, number][] = []
+            for(const pos of polygon)
+            {
+                placePoly.push([center_pos[0] + -pos[2], center_pos[1] + pos[0]]);
+            }
 
-                                // navigate to auctions page on success
-                                navigate("/auctions", { replace: true });
-                            } catch(e: any) {
-                                state.error = e.message;
-                            }
+            this.setState({ mapLocation: center_pos, placePoly: placePoly });
+        })
+    }
 
-                            actions.setSubmitting(false);
-                        }}
-                    >
-                        {({
-                            /*values,
-                            errors,
-                            touched,*/
-                            isSubmitting
-                        }) => { return (
-                            <Form>
-                                <div className="mb-3">
-                                    <label htmlFor="placeId" className="form-label">Place ID</label>
-                                    <Field id="placeId" name="placeId" type="number" className="form-control" aria-describedby="idHelp" disabled={isSubmitting} />
-                                    <div id="idHelp" className="form-text">The id of the place you want to create an auction for. Must be owned.</div>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="duration" className="form-label">Duration (in hours)</label>
-                                    <Field id="duration" name="duration" type="number" className="form-control" aria-describedby="durationHelp" disabled={isSubmitting} />
-                                    <div id="durationHelp" className="form-text">Time, in hours, until end price is reached. Auction begins immediately.</div>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="startPrice" className="form-label">Start Price</label>
-                                    <div className="input-group mb-3">
-                                        <span className="input-group-text">{'\uA729'}</span>
-                                        <Field id="startPrice" name="startPrice" type="number" className="form-control" aria-describedby="startPriceHelp" disabled={isSubmitting} />
+    onIdChange(e: React.ChangeEvent<any>) {
+        const place_id = e.target.value;
+
+        this.panMapToPlace(place_id);
+    };
+
+    componentDidMount() {
+        this.panMapToPlace(0);
+    }
+
+    render() {
+        return (
+            <div className="container text-start pt-4">
+                <div className='row'>
+                    <div className='col-lg-2 col-md-0'></div>
+                    <div className='col-lg-4 col-md-6'>
+                        <h2>Create Auction</h2>
+                        <Formik
+                            initialValues={this.initialValues}
+                            onSubmit={async (values, actions) => {
+                                //console.log({ values, actions });
+                                //alert(JSON.stringify(values, null, 2));
+
+                                // TODO: validation
+
+                                try {
+                                    await DutchAuction.createAuction(new BigNumber(values.placeId), values.startPrice, values.endPrice, values.duration);
+
+                                    // navigate to auctions page on success
+                                    const navigate = useNavigate();
+                                    navigate("/auctions", { replace: true });
+                                } catch(e: any) {
+                                    this.setState({ error: e.message});
+                                }
+
+                                // clear error state
+                                this.setState({ error: ''});
+
+                                actions.setSubmitting(false);
+                            }}
+                        >
+                            {({
+                                /*values,
+                                errors,
+                                touched,*/
+                                isSubmitting,
+                                handleChange
+                            }) => { return (
+                                <Form>
+                                    <div className="mb-3">
+                                        <label htmlFor="placeId" className="form-label">Place ID</label>
+                                        <Field id="placeId" name="placeId" type="number" className="form-control" aria-describedby="idHelp" disabled={isSubmitting} onChange={(e: React.ChangeEvent<any>) => { this.onIdChange(e); handleChange(e);}}/>
+                                        <div id="idHelp" className="form-text">The id of the place you want to create an auction for. Must be owned.</div>
                                     </div>
-                                    <div id="startPriceHelp" className="form-text">The starting price for the auction. Must be &gt; end price.</div>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="endPrice" className="form-label">End Price</label>
-                                    <div className="input-group mb-3">
-                                        <span className="input-group-text">{'\uA729'}</span>
-                                        <Field id="endPrice" name="endPrice" type="number" className="form-control" aria-describedby="endPriceHelp" disabled={isSubmitting} />
+                                    <div className="mb-3">
+                                        <label htmlFor="duration" className="form-label">Duration (in hours)</label>
+                                        <Field id="duration" name="duration" type="number" className="form-control" aria-describedby="durationHelp" disabled={isSubmitting} />
+                                        <div id="durationHelp" className="form-text">Time, in hours, until end price is reached. Auction begins immediately.</div>
                                     </div>
-                                    <div id="endPriceHelp" className="form-text">The end price for the auction. Must be &lt; starting price.<br/>There is a 2.5% fee on successful swap.</div>
-                                </div>
-                                <button type="submit" className="btn btn-primary mb-3" disabled={isSubmitting}>{isSubmitting === true && (<span className="spinner-border spinner-grow-sm" role="status" aria-hidden="true"></span>)} Create Auction</button><br/>
-                                {state.error.length > 0 && ( <span className='text-danger'>Transaction failed: {state.error}</span> )}
-                            </Form>
-                        )}}
-                    </Formik>
-                </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="startPrice" className="form-label">Start Price</label>
+                                        <div className="input-group mb-3">
+                                            <span className="input-group-text">{'\uA729'}</span>
+                                            <Field id="startPrice" name="startPrice" type="number" className="form-control" aria-describedby="startPriceHelp" disabled={isSubmitting} />
+                                        </div>
+                                        <div id="startPriceHelp" className="form-text">The starting price for the auction. Must be &gt; end price.</div>
+                                    </div>
+                                    <div className="mb-3">
+                                        <label htmlFor="endPrice" className="form-label">End Price</label>
+                                        <div className="input-group mb-3">
+                                            <span className="input-group-text">{'\uA729'}</span>
+                                            <Field id="endPrice" name="endPrice" type="number" className="form-control" aria-describedby="endPriceHelp" disabled={isSubmitting} />
+                                        </div>
+                                        <div id="endPriceHelp" className="form-text">The end price for the auction. Must be &lt; starting price.<br/>There is a 2.5% fee on successful swap.</div>
+                                    </div>
+                                    <button type="submit" className="btn btn-primary mb-3" disabled={isSubmitting}>{isSubmitting === true && (<span className="spinner-border spinner-grow-sm" role="status" aria-hidden="true"></span>)} Create Auction</button><br/>
+                                    {this.state.error.length > 0 && ( <span className='text-danger'>Transaction failed: {this.state.error}</span> )}
+                                </Form>
+                            )}}
+                        </Formik>
+                    </div>
 
-                <div className='col-lg-4 col-md-6'>
-                    <h2>Map Preview</h2>
-                    <MapContainer className="mb-2" style={{height: "20rem", backgroundColor: 'white'}} center={[500, 500]} zoom={1} attributionControl={false} dragging={false} zoomControl={false} scrollWheelZoom={false} crs={L.CRS.Simple} alt="A preview map of the land">
-                        <ImageOverlay bounds={[[0, 0], [1000, 1000]]} url="/img/map.svg" />
-                    </MapContainer>
-                    <small>Note: The the Place will be transferred to the auction contract. Auctions can be cancelled, but please make sure you really intend to create the auction.</small>
+                    <div className='col-lg-4 col-md-6'>
+                        <h2>Map Preview</h2>
+                        <MapContainer className="mb-2" style={{height: "20rem", backgroundColor: 'white'}} center={[500, 500]} zoom={2} attributionControl={false} dragging={false} scrollWheelZoom={false} crs={L.CRS.Simple} alt="A preview map of the land">
+                            <ImageOverlay bounds={[[0, 0], [1000, 1000]]} url="/img/map.svg" />
+                            <MapSetCenter center={this.state.mapLocation}/>
+                            <Circle center={this.state.mapLocation} radius={1.5} color='#d58195' fillColor='#d58195' fill={true} fillOpacity={1} />
+                            <Polygon positions={this.state.placePoly} color='#d58195' weight={10} lineCap='square'/>
+                        </MapContainer>
+                        <small>Note: The the Place will be transferred to the auction contract. Auctions can be cancelled, but please make sure you really intend to create the auction.</small>
+                    </div>
+                    <div className='col-lg-2 col-md-0'></div>
                 </div>
-                <div className='col-lg-2 col-md-0'></div>
             </div>
-        </div>
-    );
+        );
+    }
 };
