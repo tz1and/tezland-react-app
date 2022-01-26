43e9268e0776ea5e4b6fb28ab2184e9c3c31708f
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-01-26 15:17:09 +0100

    close icon color fix

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -13,18 +13,18 @@ type NotificationProps = {
 
 export const Notification: React.FC<NotificationProps> = (props: NotificationProps) => {
     let color = "bg-" + props.data.type;
-    let textColor = props.data.type === "info" || props.data.type === "warning" ? "text-dark" : "text-white";
+    let textColor = props.data.type === "info" || props.data.type === "warning" ? "dark" : "white";
     //let textColor = "text-white";
 
     return (
-        <div className={`toast align-items-center border-0 show ${textColor} ${color}`} role="alert" aria-live="assertive" aria-atomic="true">
+        <div className={`toast align-items-center border-0 show text-${textColor} ${color}`} role="alert" aria-live="assertive" aria-atomic="true">
             <div className="d-flex">
                 <div className="toast-body flex-grow-1" style={{whiteSpace: "pre-wrap"}}>
                     <p className='mb-2 fw-bolder'>{props.data.title}</p>
                     <hr className='my-2'/>
                     {props.data.body}
                 </div>
-                <button type="button" className="btn-close btn-close-white me-2 mt-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                <button type="button" className={`btn-close btn-close-${color} me-2 mt-2 m-auto"`} data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
         </div>
     )
