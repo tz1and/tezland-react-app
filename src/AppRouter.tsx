b97660e8b942ba62156510a03e17efff4e72c66d
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-02-02 14:56:20 +0100

    warnings in app router

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,16 +1,16 @@
 import React from 'react'; // we need this to make JSX compile
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-//import Explore from './components/Explore';
+/*import Explore from './components/Explore';
+import Auctions from './routes/Auctions';
+import { CreateAuctionFormW } from './forms/CreateAuction';
+import Map from './routes/Map';*/
 import ComingSoon from './routes/ComingSoon';
 import SiteLayout from './routes/SiteLayout';
 import Frontpage from './routes/Frontpage';
-import Auctions from './routes/Auctions';
 import Faq from './routes/Faq';
 import Docs from './routes/Docs';
-import Map from './routes/Map';
 import GenerateMap from './routes/GenerateMap';
 import PageNotFound from './routes/PageNotFound';
-import { CreateAuctionFormW } from './forms/CreateAuction';
 import { TezosWalletProvider } from './components/TezosWalletContext'
 import { isDev } from './utils/Utils';
 
