295ee83c045d12a56a7f47a33564f2d8dc74347d
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-03-19 15:56:35 +0100

    EditPlace: only show permissions if owner.

--- src/forms/EditPlace.tsx
+++ src/forms/EditPlace.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTezosWalletContext } from '../components/TezosWalletContext';
 import { PlaceAddPermissionsForm } from './PlaceAddPermissions';
 import { PlacePropertiesForm } from './PlaceProperties';
 import { PlaceRemovePermissionsForm } from './PlaceRemovePermissions';
@@ -11,6 +12,10 @@ type EditPlaceProps = {
 }
 
 export const EditPlace: React.FC<EditPlaceProps> = (props) => {
+    const context = useTezosWalletContext();
+
+    const is_owner = context.walletPHK() === props.placeOwner;
+
     return (
         <div className='p-4 m-4 bg-light bg-gradient border-0 rounded-3 text-dark position-relative'>
             <button type="button" className="p-3 btn-close position-absolute top-0 end-0" aria-label="Close" onClick={() => props.closeForm(true)} />
@@ -19,23 +24,23 @@ export const EditPlace: React.FC<EditPlaceProps> = (props) => {
                 <li className="nav-item" role="presentation">
                     <button className="nav-link active" id="properties-tab" data-bs-toggle="tab" data-bs-target="#properties" type="button" role="tab" aria-controls="properties" aria-selected="true">Properties</button>
                 </li>
-                <li className="nav-item" role="presentation">
+                { is_owner ? <li className="nav-item" role="presentation">
                     <button className="nav-link" id="add-permissions-tab" data-bs-toggle="tab" data-bs-target="#add-permissions" type="button" role="tab" aria-controls="add-permissions" aria-selected="false">Add Permissions</button>
-                </li>
-                <li className="nav-item" role="presentation">
+                </li> : null }
+                { is_owner ? <li className="nav-item" role="presentation">
                     <button className="nav-link" id="remove-permissions-tab" data-bs-toggle="tab" data-bs-target="#remove-permissions" type="button" role="tab" aria-controls="remove-permissions" aria-selected="false">Remove Permissions</button>
-                </li>
+                </li> : null }
             </ul>
             <div className="tab-content" id="myTabContent">
                 <div className="tab-pane fade show active" id="properties" role="tabpanel" aria-labelledby="properties-tab">
                     <PlacePropertiesForm placeId={props.placeId} placeOwner={props.placeOwner} groundColor={props.groundColor} />
                 </div>
-                <div className="tab-pane fade" id="add-permissions" role="tabpanel" aria-labelledby="add-permissions-tab">
+                { is_owner ? <div className="tab-pane fade" id="add-permissions" role="tabpanel" aria-labelledby="add-permissions-tab">
                     <PlaceAddPermissionsForm placeId={props.placeId} placeOwner={props.placeOwner}/>
-                </div>
-                <div className="tab-pane fade" id="remove-permissions" role="tabpanel" aria-labelledby="remove-permissions-tab">
+                </div> : null }
+                { is_owner ? <div className="tab-pane fade" id="remove-permissions" role="tabpanel" aria-labelledby="remove-permissions-tab">
                     <PlaceRemovePermissionsForm placeId={props.placeId} placeOwner={props.placeOwner}/>
-                </div>
+                </div> : null }
             </div>
         </div>
     );
