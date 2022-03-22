2d35dd38267d37802f74405468061565dd6f2cb7
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-03-22 03:37:36 +0100

    spelling

--- src/components/WalletWidget.tsx
+++ src/components/WalletWidget.tsx
@@ -29,7 +29,7 @@ export default function WalletWidget() {
                 <button className="btn btn-dark mb-auto ms-3 px-2"><i className="bi bi-wallet2"></i></button>
                 <button id="wallet-address-button" className="btn btn-light btn-outline-dark mb-auto px-2" ref={popoverRef}
                     onClick={() => navigator.clipboard.writeText(context.walletPHK())}>{truncate(context.walletPHK(), 10, '\u2026')}</button>
-                <button className="btn btn-primary mb-auto" onClick={() => context.disconnectWallet()}>Disonnect Wallet</button>
+                <button className="btn btn-primary mb-auto" onClick={() => context.disconnectWallet()}>Disconnect Wallet</button>
             </div> :
             <button className="btn btn-success mb-auto ms-3" onClick={() => context.connectWallet()}>Connect Wallet</button>
     );
