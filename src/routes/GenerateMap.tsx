07d136d0dba7ed4a4d4b388f0ae6d6dc9393a1a4
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-03-23 10:27:30 +0100

    mapgen

--- src/routes/GenerateMap.tsx
+++ src/routes/GenerateMap.tsx
@@ -174,27 +174,27 @@ export default class GenerateMap extends React.Component<GenerateMapProps, Gener
         const walletphk = this.context.walletPHK();
         assert(walletphk === "tz1andXaJQEfu6DCGDa7dyDJRXqQZXjvdxNA", "Not admin!");
 
-        const known_places: number[] = Array.from({length: 95}, (x, i) => i);
+        const last_batch_id = 94 + 1;
+        const last_minted_place_id = (await Contracts.countPlacesView(this.context)).minus(1).toNumber();
+
+        const known_places: number[] = Array.from({length: last_minted_place_id - last_batch_id + 1}, (x, i) => last_batch_id + i);
         const exclude_places: Set<number> = new Set([
-            0, // spawn
-            1, // me
-            3, // spacegoodies
-            2,4,5, // reserved
-            8, // too small
-            10, // fyin
-            20, // kraznik
-            23, // public
-            25, // dr grackle
-            28, // public
-            34, 37, // too small
-            36, // public
-            41, 43, 46, 47, // too small
-            48, // public
-            52, 54, 58, 64, 65, 71, // too small
-            69, // public
-            79, // too small
-            82, // PureSpider
-            87, 90, 94 // public
+            95, // public
+            106, 110, // too small
+            112, 113, // public
+            117, // public
+            118, // too small
+            122, // public
+            129, // too small
+            138, 139, // too small
+            142, // too small
+            147, // public
+            156, // too small
+            160, // public
+            164, // too small
+            169, // too small
+            172, // too small
+            174, // too small
         ]);
 
         const auction_id_list: number[] = [];
@@ -573,6 +573,8 @@ export default class GenerateMap extends React.Component<GenerateMapProps, Gener
                 for (const curb of district.curbs)
                     draw.line(curb.pointsToArray()).fill('green').stroke('green').attr({'stroke-width': 0.5});*/
             }
+
+            console.log("lot_counter", lot_counter);
 
             for (const bridge of worldgen.bridges) {
                 draw.line(bridge.bridge_path[0].asArray().concat(bridge.bridge_path[1].asArray())).stroke(bridgeColor).attr({'stroke-width': 10});
