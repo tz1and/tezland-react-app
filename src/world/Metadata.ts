cfe74cdd5649ce6c3329355462fd32c5e216adce
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-03-24 11:08:17 +0100

    Metadata: added getPlaceMetadataBatch

--- src/world/Metadata.ts
+++ src/world/Metadata.ts
@@ -20,6 +20,50 @@ export default class Metadata {
             return dbstorage;*/
     }
 
+    public static async getPlaceMetadataBatch(token_id_start: number, token_id_end: number) {
+        const places_to_fetch = [];
+
+        for (let token_id = token_id_start; token_id <= token_id_end; ++token_id) {
+            // Try to read the token metadata from storage.
+            let tokenMetadata = await Metadata.Storage.loadObject(token_id, "placeMetadata");
+
+            // If it doesn't exist, add to fetch array.
+            if(!tokenMetadata) places_to_fetch.push(token_id);
+        }
+
+        if (places_to_fetch.length === 0) return;
+
+        Logging.Info(`batch fetching ${places_to_fetch.length} places from indexer`);
+
+        const chunk_size = 50;
+        for (let i = 0; i < places_to_fetch.length; i += chunk_size) {
+            const chunk = places_to_fetch.slice(i, i + chunk_size);
+
+            const data = await fetchGraphQL(`
+                query getPlaceTokenMetadataBatch($ids: [bigint!]) {
+                    placeToken(where: { id: { _in: $ids } }) {
+                        id
+                        name
+                        description
+                        placeType
+                        buildHeight
+                        borderCoordinates
+                        centerCoordinates
+                        thumbnailUri
+                        minterId
+                    }
+                }`, "getPlaceTokenMetadataBatch", { ids: chunk });
+
+            for (const placeToken of data.placeToken) {
+                // fix up border and center coords
+                placeToken.borderCoordinates = JSON.parse(placeToken.borderCoordinates);
+                placeToken.centerCoordinates = JSON.parse(placeToken.centerCoordinates);
+
+                await Metadata.Storage.saveObject(placeToken.id, "placeMetadata", placeToken);
+            }
+        }
+    }
+
     public static async getPlaceMetadata(token_id: number): Promise<any> {
         // Try to read the token metadata from storage.
         let tokenMetadata = await Metadata.Storage.loadObject(token_id, "placeMetadata");
