84fadc46100a955cf6c61c326602ebda1c6d5255
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-03-29 20:56:33 +0200

    added SpwanSelectWidget

---
+++ src/components/SpawnSelectWidget.tsx
@@ -0,0 +1,112 @@
+import TezosWalletContext from "./TezosWalletContext";
+import { Popover } from 'bootstrap';
+import React from "react";
+import { fetchPlaces } from "../ipfs/graphql";
+import AppSettings from "../storage/AppSettings";
+import assert from "assert";
+
+type SpawnSelectProps = {
+};
+
+type SpawnSelectState = {
+    userPlaces: any[];
+    currentIsDefault: boolean;
+}
+
+export default class SpawnSelectWidget extends React.Component<SpawnSelectProps, SpawnSelectState> {
+    static override contextType = TezosWalletContext;
+    override context!: React.ContextType<typeof TezosWalletContext>;
+
+    private popoverDefaultRef = React.createRef<HTMLButtonElement>();
+    private popoverTeleportRef = React.createRef<HTMLButtonElement>();
+    private selectRef = React.createRef<HTMLSelectElement>();
+
+    private popoverDefault: Popover | null = null;
+    private popoverTeleport: Popover | null = null;
+
+    constructor(props: SpawnSelectProps) {
+        super(props);
+        this.state = {
+            userPlaces: [],
+            currentIsDefault: true
+        };
+    }
+
+    private walletChangeListener = () => {
+        // TODO: allow Browser to cache this by setting some max age or something
+        fetchPlaces(this.context).then((res) => {
+            this.setState({userPlaces: res}, () => {
+                assert(this.selectRef.current);
+                this.selectRef.current.value = AppSettings.defaultSpawn.value;
+            });
+        });
+    }
+
+    override componentDidMount() {
+        this.context.walletEvents().addListener("walletChange", this.walletChangeListener);
+
+        this.walletChangeListener();
+
+        if(this.popoverDefaultRef.current && this.popoverTeleportRef.current) {
+            this.popoverDefault = new Popover(this.popoverDefaultRef.current, {
+                content: "Set default spawn.",
+                placement: 'bottom',
+                trigger: 'hover'
+            });
+
+            this.popoverTeleport = new Popover(this.popoverTeleportRef.current, {
+                content: "Teleport to this location.",
+                placement: 'bottom',
+                trigger: 'hover'
+            });
+        }
+    }
+    
+    override componentWillUnmount() {
+        this.popoverDefault?.dispose();
+        this.popoverTeleport?.dispose();
+
+        this.context.walletEvents().removeListener("walletChange", this.walletChangeListener);
+    }
+
+    private setDefaultSpawn = () => {
+        assert(this.selectRef.current);
+
+        AppSettings.defaultSpawn.value = this.selectRef.current.value;
+
+        this.setState({currentIsDefault: true});
+    }
+
+    private teleportTo = () => {
+
+    }
+
+    private changeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const selected = e.target.value;
+
+        this.setState({ currentIsDefault: selected === AppSettings.defaultSpawn.value });
+    }
+
+    override render(): React.ReactNode {
+        const listElements: JSX.Element[] = []
+        for (var place of this.state.userPlaces) {
+            listElements.push(<option key={place.tokenId} value={"place" + place.tokenId}>Place #{place.tokenId}</option>);
+        }
+
+        // TODO: don't hardcode number of districts, load them from DistrictDefinition.
+        return (
+            <div className="btn-group" role="group" aria-label="Basic example">
+                <button className="btn btn-dark mb-auto ms-3 px-2"><i className="bi bi-bullseye"></i></button>
+                <select className="form-select rounded-0" aria-label="Default select example" ref={this.selectRef} defaultValue={AppSettings.defaultSpawn.value} onChange={this.changeSelect}>
+                    <option key="d1" value="district1">District #1</option>
+                    <option key="d2" value="district2">District #2</option>
+                    <option key="d3" value="district3">District #3</option>
+                    <option key="d4" value="district4">District #4</option>
+                    {listElements}
+                </select>
+                <button className={`btn btn-${this.state.currentIsDefault ? "success" : "secondary"} mb-auto`} onClick={this.setDefaultSpawn} ref={this.popoverDefaultRef}><i className="bi bi-check2"></i></button>
+                <button className="btn btn-light mb-auto" onClick={this.teleportTo} ref={this.popoverTeleportRef}><i className="bi bi-arrow-right"></i></button>
+            </div>
+        );
+    }
+}
--- src/forms/Instructions.tsx
+++ src/forms/Instructions.tsx
@@ -1,6 +1,7 @@
 import { Popover } from 'bootstrap';
 import React, { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import SpawnSelectWidget from '../components/SpawnSelectWidget';
 import WalletWidget from '../components/WalletWidget';
 import { FormNames } from '../world/AppControlFunctions';
 
@@ -44,6 +45,7 @@ export const Instructions: React.FC<InstructionsProps> = (props) => {
                 <button className='btn btn-light ms-3 fs-4' onClick={() => { props.loadForm('settings') } }><i className="bi bi-gear-fill"></i></button>
                 <button className='btn btn-light ms-3 fs-4' ref={popoverRef} onClick={() => { copyLocationAddress(); } }><i className="bi bi-share-fill"></i></button>
                 <WalletWidget/>
+                <SpawnSelectWidget/>
             </div>
             <div id="explore-instructions" onClick={() => props.closeForm(false)}>
                 <img src="/logo_header.png" className='mb-4' style={{filter: "invert(1)", height: "calc(20px + 8vmin)"}} alt="tz1and" />
