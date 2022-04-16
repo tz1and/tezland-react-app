37d4d3a995d6e439e350e4cb0c69ace9346043cb
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-04-16 23:02:22 +0200

    link image-framer in mint form

--- src/forms/MintForm.tsx
+++ src/forms/MintForm.tsx
@@ -300,8 +300,12 @@ export class MintFrom extends React.Component<MintFormProps, MintFormState> {
                                             <div className="mb-3">
                                                 <label htmlFor="itemFile" className="form-label">3D Model file</label>
                                                 <Field id="itemFile" name="itemFile" className="form-control" aria-describedby="fileHelp" component={CustomFileUpload} disabled={isSubmitting} />
-                                                <div id="fileHelp" className="form-text">Only glb models are supported.<br/>Self-contained gltf files will also work.<br/>
-                                                Current (default, soft) limit: {AppSettings.triangleLimit.defaultValue} triangles, {AppSettings.fileSizeLimit.defaultValue / 1024 / 1024} Mb</div>
+                                                <div id="fileHelp" className="form-text">
+                                                    Only glb models are supported.<br/>
+                                                    Self-contained gltf files will work, too.<br/>
+                                                    <a href="https://framer.tz1and.com/" target="_blank" rel="noreferrer">3D-framed images are also supported.</a><br/>
+                                                    Current (default, soft) limit: {AppSettings.triangleLimit.defaultValue} triangles, {AppSettings.fileSizeLimit.defaultValue / 1024 / 1024} Mb
+                                                </div>
                                                 <ErrorMessage name="itemFile" children={this.errorDisplay}/>
                                                 {touched.itemFile && this.state.modelLimitWarning && <small className="bg-warning text-dark rounded-1 my-1 p-1">
                                                     <i className="bi bi-exclamation-triangle-fill"></i> {this.state.modelLimitWarning}</small>}
