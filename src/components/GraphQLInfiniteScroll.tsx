16800095e56bc3c02b3dbef8630a286b1e19a432
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-06-24 21:51:06 +0200

    Event page and infinitescroll component

---
+++ src/components/GraphQLInfiniteScroll.tsx
@@ -0,0 +1,87 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+type GraphQLInfiniteScrollProps = {
+    fetchDataFunc(dataOffset: number, fetchAmount: number): Promise<any>;
+    handleClick?(item_id: number, quantity: number): void;
+    fetchAmount: number;
+    component: React.ElementType;
+};
+
+type GraphQLInfiniteScrollState = {
+    itemMap: Map<number, any>;
+    moreData: boolean;
+    itemOffset: number;
+}
+
+
+export const GraphQLInfiniteScroll: React.FC<GraphQLInfiniteScrollProps> = (props) => {
+
+    const [state, setState] = useState<GraphQLInfiniteScrollState>({
+        itemMap: new Map(),
+        moreData: false,
+        itemOffset: 0
+    });
+
+    const [firstFetchDone, setFirstFetchDone] = useState<boolean>(false);
+    const [error, setError] = useState<string>();
+
+    const fetchData = useCallback(() => {
+        props.fetchDataFunc(state.itemOffset, props.fetchAmount).then((res) => {
+            for (const r of res) state.itemMap.set(r.token.id, r);
+            const more_data = res.length === props.fetchAmount;
+
+            setState({
+                itemOffset: state.itemOffset + props.fetchAmount,
+                moreData: more_data,
+                itemMap: state.itemMap
+            });
+        }).catch(e => {
+            setError("Failed to fetch data" + e);
+        });
+    }, [props, state]);
+
+    const fetchMoreData = () => {
+        if(!error && firstFetchDone && state.moreData) {
+            fetchData();
+        }
+    }
+
+    /*const handleBurn = (item_id: number) => {
+        // TODO: modal version of transfer dialog
+        //this.props.burnItemFromInventory(item_id);
+    }
+
+    const handleTransfer = (item_id: number) => {
+        // TODO: modal version of burn dialog
+        //this.props.transferItemFromInventory(item_id);
+    }*/
+
+    useEffect(() => {
+        if(!firstFetchDone) {
+            fetchData();
+            setFirstFetchDone(true);
+        }
+    }, [firstFetchDone, fetchData]);
+
+    const items: JSX.Element[] = []
+    if (!error) state.itemMap.forEach(item => items.push(<props.component key={item.token.id} onSelect={props.handleClick}
+        /*onBurn={isOwned ? this.handleBurn : undefined}
+        onTransfer={isOwned ? this.handleTransfer : undefined}*/
+        item_metadata={item}/>))
+
+    let content = error ? <h5 className='mt-3'>{error}</h5> : items;
+
+    return (
+        <InfiniteScroll
+            className="d-flex flex-row flex-wrap justify-content-start align-items-start"
+            dataLength={items.length} //This is important field to render the next data
+            next={fetchMoreData}
+            hasMore={state.moreData}
+            loader={<h5 className='mt-3'>Loading...</h5>}
+            scrollThreshold={0.9}
+        >
+            {content}
+        </InfiniteScroll>
+    );
+}
--- src/routes/Event.tsx
+++ src/routes/Event.tsx
@@ -1,21 +1,55 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
-import Collection from '../components/Collection';
-import Creations from '../components/Creations';
-import { Places } from '../components/Places';
-import { truncateAddress } from '../utils/Utils';
-import { Tab, Tabs } from 'react-bootstrap';
+import { useNavigate, useParams } from 'react-router-dom';
 import assert from 'assert';
+import { GraphQLInfiniteScroll } from '../components/GraphQLInfiniteScroll';
+import { fetchGraphQL } from '../ipfs/graphql';
+import { PlaceItem } from '../components/PlaceItem';
+import { Logging } from '../utils/Logging';
+import { getiFrameControl } from '../forms/DirectoryForm';
 
 type UserProps = {}
 
 const Event: React.FC<UserProps> = (props) => {
     const params = useParams();
+    const navigate = useNavigate();
 
     assert(params.eventName);
     assert(params.eventLabel);
     const eventName = params.eventName;
     const eventLabel = params.eventLabel;
+
+    const fetchPlaceData = async (dataOffset: number, fetchAmount: number): Promise<any> => {
+        const eventView = `placesEvent${eventName}`
+
+        const dataPlaceIds = await fetchGraphQL(`
+            query getEventPlaces($offset: Int!, $amount: Int!) {
+                ${eventView}(limit: $amount, offset: $offset, order_by: {id: asc}) {
+                    id
+                }
+            }`, "getEventPlaces", { amount: fetchAmount, offset: dataOffset });
+        
+        const results = dataPlaceIds[eventView];
+
+        const formatted: any[] = []
+        for (const res of results) {
+            formatted.push({token: {id: res.id}, canVisit: true});
+        }
+
+        Logging.Info(formatted)
+
+        return formatted;
+    }
+
+    const handleClick = (item_id: number, quantity: number) => {
+        const iframeControl = getiFrameControl(window);
+
+        if(iframeControl) {
+            iframeControl.teleportToLocation("place" + item_id);
+            iframeControl.closeForm(false);
+        }
+        else
+            navigate(`/explore?place_id=${item_id}`);
+    }
 
     return (
         <main>
@@ -23,6 +57,7 @@ const Event: React.FC<UserProps> = (props) => {
                 <h1>{eventLabel}</h1>
                 <h5>All Places participating in this event</h5>
 
+                <GraphQLInfiniteScroll fetchDataFunc={fetchPlaceData} handleClick={handleClick} fetchAmount={20} component={PlaceItem}/>
             </div>
         </main>
     );
