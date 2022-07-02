375c933a31de4b5f1752815de24212938cb76ac7
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-07-02 21:27:32 +0200

    add token id to inventory item

--- src/components/InventoryItem.tsx
+++ src/components/InventoryItem.tsx
@@ -70,13 +70,13 @@ export const InventoryItem: React.FC<InventoryItemProps> = (props) => {
                 <div onClick={() => props.onSelect(token_data.id, item_data.quantity)}>
                     <img src={getThumbnailUrl(item_metadata?.thumbnailUri)} width={350} height={350} className="card-img-top inventory-item-image" alt="..."/>
                     <div className="card-body">
-                        <h5 className="card-title">{name ? truncate(name, 15, '\u2026') : <span className='text-danger'>Metadata missing</span>}</h5>
+                        <h6 className="card-title">{name ? truncate(name, 19, '\u2026') : <span className='text-danger'>Metadata missing</span>}</h6>
                         <p className="card-text">x{quantity}{itemTrackedBalance}<small>/{token_data.supply}</small>{item_data.swapInfo && ` for ${mutezToTez(item_data.swapInfo.price).toNumber().toFixed(2)} \uA729`}</p>
-                        <small className="card-text"></small>
                         <p className="card-text small m-0">
                             Royalties: {token_data.royalties === 0 ? 0 : (token_data.royalties / 10).toFixed(2)}{"\u0025"}<br/>
                             Minter: </p>
                         <p className="card-text small text-muted">{truncateAddress(token_data.minterId)}</p>
+                        <p className="card-text text-end" style={{fontSize: '0.6rem'}}>Token ID: {token_data.id}</p>
                     </div>
                 </div>
             </div>
