318a4b8a321a8a708dbfacb53b7398d85b612a39
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-07-04 21:53:07 +0200

    Logging: make it work in web workers

--- src/utils/Logging.ts
+++ src/utils/Logging.ts
@@ -1,17 +1,19 @@
 import { isDev } from "./Utils"
 
+const theWindow = typeof window !== 'undefined' ? window : globalThis;
+
 export namespace Logging {
     // TODO: window or globalThis
-    export const Log = console.log.bind(window.console);
-    export const Info = console.info.bind(window.console);
-    export const Warn = console.warn.bind(window.console);
-    export const Error = console.error.bind(window.console);
+    export const Log = console.log.bind(theWindow.console);
+    export const Info = console.info.bind(theWindow.console);
+    export const Warn = console.warn.bind(theWindow.console);
+    export const Error = console.error.bind(theWindow.console);
 
-    export const LogDev = isDev() ? console.log.bind(window.console) : () => {};
-    export const InfoDev = isDev() ? console.info.bind(window.console) : () => {};
-    export const WarnDev = isDev() ? console.warn.bind(window.console) : () => {};
-    export const ErrorDev = isDev() ? console.error.bind(window.console) : () => {};
+    export const LogDev = isDev() ? console.log.bind(theWindow.console) : () => {};
+    export const InfoDev = isDev() ? console.info.bind(theWindow.console) : () => {};
+    export const WarnDev = isDev() ? console.warn.bind(theWindow.console) : () => {};
+    export const ErrorDev = isDev() ? console.error.bind(theWindow.console) : () => {};
 
-    export const Dir = console.dir.bind(window.console);
-    export const DirDev = isDev() ? console.dir.bind(window.console) : () => {};
+    export const Dir = console.dir.bind(theWindow.console);
+    export const DirDev = isDev() ? console.dir.bind(theWindow.console) : () => {};
 }
