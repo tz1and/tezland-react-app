92e864ca394c5be6317aed39c5ba40a031448099
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-07-11 15:15:24 +0200

    made index.tsx a little nicer

--- src/index.tsx
+++ src/index.tsx
@@ -9,33 +9,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import reportWebVitals from './reportWebVitals';
 import AppRouter from './AppRouter';
-import Metadata from './world/Metadata';
 import { Notification } from './components/Notification';
-import { isDev, sleep } from './utils/Utils';
 import { upgradeSettings } from './storage/AppSettings';
-import Conf from './Config';
+import Metadata from './world/Metadata';
 import { Logging } from './utils/Logging';
+import { isDev, sleep } from './utils/Utils';
+import Conf from './Config';
 
-// make sure the default settings are up to date.
-upgradeSettings();
 
-Metadata.InitialiseStorage().then(() => {
-    renderApp();
-}, (reason: any) => {
-    Logging.Error("Failed to initialise database:", reason);
-    renderApp(
-        (<div className="toast-container position-fixed bottom-0 start-50 translate-middle-x p-4" style={{zIndex: "1050"}}>
-            <Notification data={{
-                id: "storageFailure",
-                title: "Failed to open Database storage",
-                body: "The app may not function correctly.\n\nCheck the Javascript console for more details. It could also be your privacy settings (or a private tab).",
-                type: 'danger' }}/>
-        </div>)
-    );
-})
+function InitialiseApp() {
+    // make sure the default settings are up to date.
+    upgradeSettings();
 
-// TODO: Move to React 18 style
-const renderCallback = () => {
+    Metadata.InitialiseStorage().then(() => {
+        RunApp();
+    }).catch((e) => {
+        Logging.Error("Failed to initialise database:", e);
+        RunApp(false);
+    });
+}
+
+const RenderCallback = () => {
     sleep(500).then(() => {
         const loader = document.getElementById('loader-container')
         if(!loader) return;
@@ -47,7 +41,21 @@ const renderCallback = () => {
     });
 }
 
-const renderApp = (element?: JSX.Element) => {
+function RunApp(dbInitSuccess: boolean = true) {
+    // TODO: react 18 style. Can't do it now because it does silly stuff like
+    // mounting components twice in development and that breaks some stuff...
+    //const container = document.getElementById('root');
+    //const root = createRoot(container!);
+
+    const dbFailedNotify = !dbInitSuccess ?
+        <div className="toast-container position-fixed bottom-0 start-50 translate-middle-x p-4" style={{zIndex: "1050"}}>
+            <Notification data={{
+                id: "storageFailure",
+                title: "Failed to open Database storage",
+                body: "The app may not function correctly.\n\nCheck the Javascript console for more details. It could also be your privacy settings (or a private tab).",
+                type: 'danger' }}/>
+        </div> : undefined;
+
     const testnetNotify = Conf.tezos_network !== "mainnet" ?
         <div className="toast-container position-fixed bottom-0 start-50 translate-middle-x p-4" style={{zIndex: "1050"}}>
             <Notification data={{
@@ -55,22 +63,24 @@ const renderApp = (element?: JSX.Element) => {
                 title: "Live on testnet",
                 body: <span>tz1and is live on the testnet! Again! To give it a try, grab an account <a href="https://teztnets.xyz/" target="_blank" rel="noreferrer">from the faucet</a> and import it in TempleWallet.</span>,
                 type: 'info' }}/>
-        </div> : null
+        </div> : undefined;
 
+    // TODO: react 18 style
+    //root.render(...);
     ReactDOM.render(
         <React.StrictMode>
             <AppRouter>
-                {element}
+                {dbFailedNotify}
                 { isDev() ? <div className='bg-danger text-light text-center align-middle py-2 fixed-bottom'><b>DEVELOPMENT - DEVELOPMENT - DEVELOPMENT - DEVELOPMENT</b></div> : null }
                 {testnetNotify}
             </AppRouter>
         </React.StrictMode>,
         document.getElementById('root'),
-        renderCallback
+        RenderCallback
     );
 }
 
-
+InitialiseApp();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
