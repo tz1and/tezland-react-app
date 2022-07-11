36057ab7f14a7265eed911cef6a1d94ea6dfe42c
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-07-11 08:15:35 +0200

    ArtifactDownload.worker: min 2, max 8 threads

--- src/workers/ArtifactDownload.worker.ts
+++ src/workers/ArtifactDownload.worker.ts
@@ -11,7 +11,9 @@ const pool = Pool(
     () => spawn<typeof MeshPreprocessingWorkerApi>(
         new Worker(new URL("./MeshPreprocessing.worker.ts", import.meta.url),
         { name: "MeshPreprocessing.worker" })),
-    getNumLogicalCores());
+    // At least two, but at most 8 threads.
+    // Note: Pool.terminate chashes chromium if there are 16 threads.
+    Math.max(2, Math.min(8, getNumLogicalCores())));
 
 const downloadArtifact: (typeof ArtifactDownload.downloadArtifact) = async (
     token_id: BigNumber, sizeLimit: number, polygonLimit: number, maxTexRes:
