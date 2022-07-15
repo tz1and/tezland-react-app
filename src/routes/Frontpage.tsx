05de66857e7a48c05c851a56c7bb4cd55a3ea587
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-07-15 12:15:00 +0200

    frontpage styling and image changes

--- src/routes/Frontpage.tsx
+++ src/routes/Frontpage.tsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
 import './Frontpage.css'
 
-import world_screenshot from '../img/world_screenshot.png';
-import map_screenshot from '../img/map_screenshot.png';
-import community_screenshot from '../img/district4_snapshot.png';
-import tezos_ipfs from '../img/tezos_ipfs.png';
+import world_screenshot from '../img/world_screenshot.webp';
+import map_screenshot from '../img/map_screenshot.webp';
+import community_screenshot from '../img/district4_snapshot2.webp';
+import tezos_ipfs from '../img/tezos_ipfs.webp';
 
 export default function Frontpage() {
     return (
         <main>
             <div className="px-4 pt-5 my-5 text-center border-bottom">
-                <h1 className="display-4 fw-bold">A Virtual World and NFT Marketplace</h1>
+                <h1 className="display-3">A Virtual World and NFT Marketplace</h1>
                 <div className="col-lg-6 mx-auto">
                     <p className="lead mb-4">Lorem ipsum... let's build a piece of art. Expanding, not based on scarcity.</p>
                     <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-5">
@@ -29,7 +29,7 @@ export default function Frontpage() {
             <div className="container col-xxl-8 px-0 py-2">
                 <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
                     <div className="col-lg-6 mx-4 mx-md-0">
-                        <h1 className="display-5 fw-bold lh-1 mb-3">Decentralised</h1>
+                        <h1>Decentralised</h1>
                         <p className="lead">No, for reals.<br/>Everything is stored either on
                             the <a className='link-info text-decoration-none' href="https://tezos.com/" target="_blank" rel="noreferrer">Tezos</a> blockchain or
                             on <a className='link-info text-decoration-none' href="https://ipfs.io/" target="_blank" rel="noreferrer">IPFS</a>. This website is just a portal, there could be others.</p>
@@ -48,7 +48,7 @@ export default function Frontpage() {
                             height="500" loading="lazy" />
                     </div>
                     <div className="col-lg-6 mx-4 mx-md-0">
-                        <h1 className="display-5 fw-bold lh-1 mb-3">Ever Expanding</h1>
+                        <h1>Ever Expanding</h1>
                         <p className="lead">The World will be expanded at a pace that makes sense and new Places will become available regularly.</p>
                     </div>
                 </div>
@@ -57,7 +57,7 @@ export default function Frontpage() {
             <div className="container col-xxl-8 px-0 py-2">
                 <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
                     <div className="col-lg-6 mx-4 mx-md-0">
-                        <h1 className="display-5 fw-bold lh-1 mb-3">Build Together</h1>
+                        <h1>Build Together</h1>
                         <p className="lead">Imagine some clever point about collaboration here.<br/>Let's build something big, together.</p>
                         {/*<div className="d-grid gap-2 d-md-flex justify-content-md-start">
                             <a className="btn btn-primary btn-lg px-4 me-md-2">Primary</a>
@@ -78,7 +78,7 @@ export default function Frontpage() {
                             height="500" loading="lazy" />
                     </div>
                     <div className="col-lg-6 mx-4 mx-md-0">
-                        <h1 className="display-5 fw-bold lh-1 mb-3">Community</h1>
+                        <h1>Community</h1>
                         <p className="lead">Feel free to join our Discord for discussion, updates and to meet nice people.</p>
                         <a href="https://discord.gg/fbpy4QdzSp" target="_blank" rel="noreferrer" className="btn btn-primary">Join Discord</a>
                     </div>
@@ -87,7 +87,7 @@ export default function Frontpage() {
 
             <div className="my-5 text-center">
                 <div className="p-5 align-items-center border shadow-lg bg-dark text-white bg-whitelist">
-                    <h1 className="display-5 fw-bold">Get a Place</h1>
+                    <h1 className="display-4">Get a Place</h1>
                     <div className="col-lg-10 mx-auto">
                         <p className="lead">If there are any available right now :)</p>
                         <p className="lead">I did consider making Places free. Think of it as supporting development and paying the bills.</p>
