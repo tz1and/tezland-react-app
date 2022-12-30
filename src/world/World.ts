7d7ea5f24fdfa533d0ba69d3a4fafe0ab3b8de86
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2022-12-30 08:27:40 +0100

    Downgrade babylonjs to fix prod perf

--- src/world/World.ts
+++ src/world/World.ts
@@ -542,7 +542,8 @@ export class World extends BaseWorld {
         }
     }
 
-    private updateCurrentPlace(pos: DeepImmutable<Vector3>) {
+    // NOTE: For BabylonJs > 5.16.0
+    /*private updateCurrentPlace(pos: DeepImmutable<Vector3>) {
         const pickResult = this.game.scene.pickWithRay(new Ray(pos, Vector3.Up()), (mesh) => {
             return mesh.parent instanceof PlaceNode;
         });
@@ -552,6 +553,20 @@ export class World extends BaseWorld {
 
             // TODO: use normal to determine whether we are inside our out.
             if (Vector3.Dot(pickResult.getNormal()!, pickResult.ray!.direction) < 0)
+                this.game.playerController.currentPlace = pickResult.pickedMesh.parent;
+        }
+    }*/
+
+    private updateCurrentPlace(pos: DeepImmutable<Vector3>) {
+        const pickResult = this.game.scene.pickWithRay(new Ray(pos, Vector3.Forward()), (mesh) => {
+            return mesh.parent instanceof PlaceNode;
+        });
+
+        if (pickResult && pickResult.hit && pickResult.pickedMesh) {
+            assert(pickResult.pickedMesh.parent instanceof PlaceNode);
+
+            // TODO: use normal to determine whether we are inside our out.
+            if (Vector3.Dot(pickResult.getNormal()!, pickResult.ray!.direction) > 0)
                 this.game.playerController.currentPlace = pickResult.pickedMesh.parent;
         }
     }
