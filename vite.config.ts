77fa54d07f0230f157e6a4d439ab152ec5572b9e
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2023-01-04 20:45:36 +0100

    index changes for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,70 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 //import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 //import rollupPolyfillNode from 'rollup-plugin-polyfill-node'
 
-export default defineConfig({
-    resolve: {
-        alias: {
-            //assert: "assert/",
-            buffer: "buffer/",
-            crypto: "crypto-browserify",
-            //event: "event/",
-            path: "path-browserify",
-            process: "process/browser",
-            stream: "stream-browserify",
-            util: "util/",
-        },
-    },
+export default defineConfig(({mode}) => {
+    // Ref: https://github.com/Taiwan-Ebook-Lover/Taiwan-Ebook-Lover.github.io/pull/62/commits/cf27dd66280e8c21daaf3b51c594c1eea9065fdd#r886052909
+    const env = loadEnv(mode, '');
+    const htmlPlugin = () => ({
+        name: "html-transform",
+        transformIndexHtml: (html: string) =>
+            html.replace(
+                /<%=\s*([a-zA-Z_]+)\s*%>/g,
+                (_match, variableName) => env[variableName]
+            )
+    });
 
-    optimizeDeps: {
-        esbuildOptions: {
-            define: {
-                global: 'globalThis'
+    return {
+        resolve: {
+            alias: {
+                //assert: "assert/",
+                buffer: "buffer/",
+                crypto: "crypto-browserify",
+                //event: "event/",
+                path: "path-browserify",
+                process: "process/browser",
+                stream: "stream-browserify",
+                util: "util/",
             },
-            plugins: [
-                //NodeModulesPolyfillPlugin(),
-                NodeGlobalsPolyfillPlugin({
-                    buffer: true,
-                    process: true
-                }),
-                {
-                    name: 'fix-node-globals-polyfill',
-                    setup(build) {
-                        build.onResolve(
-                            { filter: /(_virtual-process-polyfill_|_buffer)\.js/ },
-                            ({ path }) => ({ path })
-                        )
+        },
+
+        optimizeDeps: {
+            esbuildOptions: {
+                define: {
+                    global: 'globalThis'
+                },
+                plugins: [
+                    //NodeModulesPolyfillPlugin(),
+                    NodeGlobalsPolyfillPlugin({
+                        buffer: true,
+                        process: true
+                    }),
+                    {
+                        name: 'fix-node-globals-polyfill',
+                        setup(build) {
+                            build.onResolve(
+                                { filter: /(_virtual-process-polyfill_|_buffer)\.js/ },
+                                ({ path }) => ({ path })
+                            )
+                        }
                     }
-                }
-            ]
-        }
-    },
+                ]
+            }
+        },
 
-    /*build: {
-        rollupOptions: {
-            plugins: [
-                rollupPolyfillNode({include: ['buffer']})
-            ]
-        }
-    },*/
+        /*build: {
+            rollupOptions: {
+                plugins: [
+                    rollupPolyfillNode({include: ['buffer']})
+                ]
+            }
+        },*/
 
-    plugins: [
-        react()
-    ]
+        plugins: [
+            htmlPlugin(),
+            react()
+        ]
+    }
 })
