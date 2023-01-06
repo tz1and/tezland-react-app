b0a6a21bf78b02deab9f5c3e7658b88efcb92518
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2023-01-06 20:57:03 +0100

    linting, deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslint from 'vite-plugin-eslint'
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 //import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
 //import rollupPolyfillNode from 'rollup-plugin-polyfill-node'
@@ -67,6 +68,7 @@ export default defineConfig(({mode}) => {
 
         plugins: [
             htmlPlugin(),
+            eslint(),
             react()
         ]
     }
