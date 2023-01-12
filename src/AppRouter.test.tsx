486289de269807d7b88a797cee8ddb10b50ab9bf
852Kerfunkle <852Kerfunkle@users.noreply.github.com>
2023-01-12 22:13:02 +0100

    add lint, remove unused config

--- src/AppRouter.test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import AppRouter from './AppRouter';
-
-test('renders learn react link', () => {
-  // TODO
-});
